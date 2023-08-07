f3b9852b1817cf62ec9e56109c152ed035597651
SandyMiaohs <sandymiaohs@gmail.com>
2023-08-08 00:04:00 +1200

    Adding the API

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,15 @@
 import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack";
+import { StorageStack } from "./stacks/StorageStack";
+import { ApiStack } from "./stacks/ApiStack";
 
 export default {
   config(_input) {
     return {
-      name: "app",
+      name: "notes",
       region: "ap-southeast-2",
     };
   },
   stacks(app) {
-    app.stack(API);
-  }
+    app.stack(StorageStack).stack(ApiStack);
+  },
 } satisfies SSTConfig;
